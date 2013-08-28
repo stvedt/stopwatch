a26c9d918bec412ae0a9bd103865243d444039de
Stephen Tvedt <stephen@stephentvedt.com>
2013-08-28 17:19:47 -0400

    modifications with keith

--- static/js/main.js
+++ static/js/main.js
@@ -4,11 +4,11 @@ var Stopwatch = function($){
 
     // Save reference to App object scope
     var _this = this;
-    _this.startTime;
-    _this.updateInterval;
-    _this.clock;
+    _this.startTime = null;
+    _this.updateInterval = null;
+    _this.clock = null;
     _this.pausedTime = 0;
-    
+
     _this.elapsedTime = 0;
     _this.paused = false;
     _this.laps = $('ol#laps').empty();
@@ -26,17 +26,41 @@ var Stopwatch = function($){
     _this.stopBtn.click(function(){ _this.stop(); });
     _this.pauseBtn.click(function(){ _this.pause(); });
     _this.lapBtn.click(function(){ _this.lap(); });
-    _this.resetBtn.click(function(){ _init.call(this); });
+    _this.resetBtn.click(function(){ _this.reset(); });
     // Single point of entry - called immediately after instantiation
-    function _init(){
+    var _init = function(){
         console.log("-> App::init()");
-        //I want the reset function to fire this _init()
-        _this.time.text('00:00:00.0');
-        _this.laps.empty();
-
     }
 
     // Application methods
+
+    _this.updateTime = function(){
+
+        //scope of set interval throws this.  This is an instance/public function.
+
+        //this loses scope of object
+        var clock = new Date();
+        var currentTime = clock.getTime();
+        _this.elapsedTime = _this.pausedTime + ( currentTime - _this.startTime );
+
+        var seconds = _this.elapsedTime/1000;
+        var minutes = seconds/60;
+        var hours = minutes/60;
+
+        seconds = (seconds%60).toFixed(1); //just seconds not totol seconds  Format: ##.#
+        if(seconds < 10) seconds = _this.zeros(seconds);
+
+        minutes = (minutes%60); //just minutes not total minutes    Format: ##
+        minutes =  (minutes >= 1) ? parseInt(minutes) : 0;
+        if(minutes < 10) minutes = _this.zeros(minutes);
+
+        hours = (hours >= 1) ? parseInt(hours): 0;
+        if(hours < 10) hours = _this.zeros(hours);
+
+        //console.log(_this.time);
+        $(_this.time).text(hours + ":" + minutes + ":" + seconds);
+
+    };
 
     /* ************************************************************************** */
     /* At the end of App instantiation, call the init function of the App object. */
@@ -44,11 +68,9 @@ var Stopwatch = function($){
     _init.call(this);
 };
 
+//static variables go here (public)
+
 // Application functions/procedures
-Stopwatch.prototype.test = function(){
-    //console.log(this.pausedTime);
-    return Stopwatch.pausedTime;
-};
 
 Stopwatch.prototype.btnAble= function(able){
 
@@ -71,39 +93,10 @@ Stopwatch.prototype.zeros = function(num){
     return num;
 };
 
-Stopwatch.prototype.updateTime = function(){
-    
-    //this loses scope of object
-    var clock = new Date();
-    var currentTime = clock.getTime();
-    //console.log( _this.pausedTime );
-    _this.elapsedTime = _this.pausedTime + ( currentTime - _this.startTime );
-    
-    var seconds = _this.elapsedTime/1000;
-    var minutes = seconds/60;
-    var hours = minutes/60;
-    
-    //console.log(_this.pausedTime + ' ' + currentTime + ' ' + _this.startTime);
-    seconds = (seconds%60).toFixed(1); //just seconds not totol seconds  Format: ##.#
-    if(seconds < 10) seconds = _this.zeros(seconds);
-    
-    minutes = (minutes%60); //just minutes not total minutes    Format: ##
-    minutes =  (minutes >= 1) ? parseInt(minutes) : 0;
-    if(minutes < 10) minutes = _this.zeros(minutes);
-    
-    hours = (hours >= 1) ? parseInt(hours): 0;
-    if(hours < 10) hours = _this.zeros(hours);
-    
-    //console.log(_this.time);
-    $(_this.time).text(hours + ":" + minutes + ":" + seconds);       
-    
-};
-
 Stopwatch.prototype.start = function(){
-    _this = this;
+    var _this = this;
     _this.btnAble('disable' , _this.startBtn, _this.resetBtn );
     _this.btnAble('enable', _this.stopBtn, _this.lapBtn, _this.pauseBtn );
-    _this.laps.empty();
 
     _this.clock = new Date();
     _this.startTime = _this.clock.getTime();
@@ -112,7 +105,7 @@ Stopwatch.prototype.start = function(){
 };
 
 Stopwatch.prototype.stop = function(){
-    _this = this;
+    var _this = this;
     clearInterval(_this.updateInterval);
     _this.btnAble('enable' , _this.startBtn, _this.resetBtn );
     _this.btnAble('disable', _this.stopBtn, _this.lapBtn, _this.pauseBtn );
@@ -124,6 +117,7 @@ Stopwatch.prototype.stop = function(){
 };
 
 Stopwatch.prototype.pause = function(){
+    var _this = this;
     if (_this.paused === false){
         _this.paused = true;
         _this.pauseBtn.text('Resume');
@@ -139,22 +133,23 @@ Stopwatch.prototype.pause = function(){
 };
 
 Stopwatch.prototype.lap = function (){
+    var _this = this;
     var lapTime = $('#time').html();
     lapTime = $('<li />').text(lapTime);
     _this.laps.append(lapTime)
+};
+
+Stopwatch.prototype.reset = function(){
+    var _this = this;;
+    _this.time.text('00:00:00.0');
+    _this.laps.empty();
 };
 
 Stopwatch.prototype.toString = function(){
     return '[object App]';
 };
 
-Stopwatch.prototype.reset = function(){
-    _this._init(); //Line isn't firing
-};
-
 // Instantiate application in $(document).ready()
 $(function(jquery){
-    $(document).ready(function(){
-        window.stopwatch = new Stopwatch(jquery);
-    });
+    window.stopwatch = new Stopwatch(jquery);
 });
